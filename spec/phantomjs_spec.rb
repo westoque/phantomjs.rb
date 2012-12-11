7490e99b8b1a197b9672a22a32c5d572ff966514
jejacks0n <jejacks0n@gmail.com>
2012-12-11 10:32:42 -0700

    Added the ability to pass a block to run that will be called for each line of output.
    We're using Phantomjs.rb in a test runner, and needed this functionality to output each line as the specs are being executed, instead of waiting until the end for the output.

--- spec/phantomjs_spec.rb
+++ spec/phantomjs_spec.rb
@@ -7,5 +7,14 @@ describe Phantomjs do
       result = Phantomjs.run(script, 'foo1', 'foo2')
       result.should eq("bar foo1 foo2\n")
     end
+    
+    it "accepts a block that will get called for each line of output" do
+      line = ''
+      script = File.expand_path('./spec/runner.js')
+      Phantomjs.run(script, 'foo1', 'foo2') do |l|
+        line << l
+      end
+      line.should eq("bar foo1 foo2\n")
+    end
   end
 end
